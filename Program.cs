a2575bd5a19fbf4babcb66418b00125794ce9b48
BlizzieM <tuisku.penttila@outlook.com>
2023-05-26 13:05:24 +0300

    Customiseable language input/output

--- Program.cs
+++ Program.cs
@@ -7,19 +7,23 @@ class Program
     {
         string? aLine = null;
         string? iLang = null;
+        string? oLang = null;
 
-        Console.WriteLine("Enter 2 character language code:");
+        Console.WriteLine("Enter 2 character language code for languate to translate from:");
         iLang = Console.ReadLine();
 
-        Console.WriteLine("Enter Text to be translated into spanish:");
+        Console.WriteLine("Enter 2 character language code for languate to translate to:");
+        oLang = Console.ReadLine();
+
+        Console.WriteLine("Enter Text to be translated:");
         aLine = Console.ReadLine();
         Console.WriteLine("Translating...");
 
         SendText translator = new SendText();
         
-        await Task.Delay(1000);
+        await Task.Delay(2000);
 
-        string result = await translator.Translate(aLine,iLang);
+        string result = await translator.Translate(aLine,iLang,oLang);
 
         Console.WriteLine(result);
     }
