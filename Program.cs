cf6d2f2623864be4f20237c6186f5ed61047b599
BlizzieM <tuisku.penttila@outlook.com>
2023-05-26 12:53:50 +0300

    Change input language

--- Program.cs
+++ Program.cs
@@ -6,16 +6,20 @@ class Program
     static async Task Main(string[] args)
     {
         string? aLine = null;
+        string? iLang = null;
 
-        Console.WriteLine("Enter English Text to be translated into spanish:");
+        Console.WriteLine("Enter 2 character language code:");
+        iLang = Console.ReadLine();
+
+        Console.WriteLine("Enter Text to be translated into spanish:");
         aLine = Console.ReadLine();
         Console.WriteLine("Translating...");
 
         SendText translator = new SendText();
         
-        await Task.Delay(2000);
+        await Task.Delay(1000);
 
-        string result = await translator.Translate(aLine);
+        string result = await translator.Translate(aLine,iLang);
 
         Console.WriteLine(result);
     }
