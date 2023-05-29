f5e0ad1bbf21bc0d5cb32981bdcc5c52cb0d3001
BlizzieM <tuisku.penttila@outlook.com>
2023-05-29 11:57:21 +0300

    Refactorings and looping

--- Program.cs
+++ Program.cs
@@ -3,29 +3,60 @@ using Newtonsoft.Json;
 
 class Program
 {
+
     static async Task Main(string[] args)
     {
         string? aLine = null;
         string? iLang = null;
         string? oLang = null;
+        bool shouldRun = true;
 
-        Console.WriteLine("Enter 2 character language code for languate to translate from:");
-        iLang = Console.ReadLine();
 
-        Console.WriteLine("Enter 2 character language code for languate to translate to:");
-        oLang = Console.ReadLine();
 
-        Console.WriteLine("Enter Text to be translated:");
-        aLine = Console.ReadLine();
-        Console.WriteLine("Translating...");
+        do
+        {
+            Console.WriteLine("Enter 2 character language code for languate to translate from:");
+            iLang = Console.ReadLine();
 
-        SendText translator = new SendText();
-        
-        await Task.Delay(2000);
+            Console.WriteLine("Enter 2 character language code for languate to translate to:");
+            oLang = Console.ReadLine();
 
-        string result = await translator.Translate(aLine,iLang,oLang);
+            Console.WriteLine("Enter Text to be translated:");
+            aLine = Console.ReadLine();
+            Console.WriteLine("Translating...");
 
-        Console.WriteLine(result);
+            SendText translator = new SendText();
+
+            string result = await translator.Translate(aLine, iLang, oLang);
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("Want to translate another line? (Y)es/(N)o:");
+            
+            bool continueCheck = true;
+
+            do
+            {
+                string? keepTranslating = Console.ReadLine();
+                if (keepTranslating == "Y" || keepTranslating == "y")
+                {
+                    continueCheck = false;
+                }
+
+                else if (keepTranslating == "N" || keepTranslating == "n")
+                {
+                    shouldRun = false;
+                    continueCheck = false;
+                }
+
+                else
+                {
+                    Console.WriteLine("Please enter a valid character!");
+                }
+            }
+            while (continueCheck == true);
+        }
+        while (shouldRun == true);
     }
 
 }
