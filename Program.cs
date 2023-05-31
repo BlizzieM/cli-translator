2361549cbf5fdd560d727217f1311970f0e93ab2
BlizzieM <tuisku.penttila@outlook.com>
2023-05-31 13:51:54 +0300

    swapped from parser to node based approach

--- Program.cs
+++ Program.cs
@@ -1,85 +1,107 @@
 ﻿using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using Newtonsoft.Json;
 
-public record struct Transl
+namespace translator
 {
-       public string? text { get; set; }
+    //Ohjelma tää perkeleestä
+    //Softa sieltä ja syvältä
+    //Pyydän sinut tulkaamahan
+    //jsoneita tavaamahan
+    //ja käännöksiä näyttämähän
+    //tekstejä piirtämähän
+    //tai roskiksehen sinut heitän
+    //matosille marehdittivän
 
-    public string? to { get; set; }
-}
-
-class Program
-{
-
-    static void parseJson(string res)
+    //friend recommended that I try to hex this with a poem
+    //it didn't fix the issues
+    class Translation
     {
-        var result = new List<Transl>();
 
-        result = System.Text.Json.JsonSerializer.Deserialize<List<Transl>>(String.Format(@"{0}", res));
+        public List<TextTo>? translation { get; set; }
 
-        if (result != null && result.Count > 0)
-        {
-            foreach (var Transl in result)
-            {
-                Console.WriteLine($"{Transl.text}");
-            }
-        }
     }
 
-    static async Task Main(string[] args)
+    class TextTo
     {
-        string? aLine = null;
-        string? iLang = null;
-        string? oLang = null;
-        bool shouldRun = true;
 
+        public string? text { get; set; }
 
+        public string? to { get; set; }
+    }
 
-        do
+    class Program
+    {
+
+        static void parseJson(string res)
         {
-            Console.WriteLine("Enter 2 character language code for languate to translate from:");
-            iLang = Console.ReadLine();
+            string json = res;
 
-            Console.WriteLine("Enter 2 character language code for languate to translate to:");
-            oLang = Console.ReadLine();
+            JsonNode rootNode = JsonNode.Parse(json)!;
+            JsonArray translations = rootNode[0]["translations"].AsArray();
 
-            Console.WriteLine("Enter Text to be translated:");
-            aLine = Console.ReadLine();
-            Console.WriteLine("Translating...");
+            foreach (var translation in translations)
+            {
+                Console.WriteLine(translation["text"].AsValue());
+            }
+        }
 
-            SendText translator = new SendText();
 
-            string result = await translator.Translate(aLine, iLang, oLang);
+        static async Task Main(string[] args)
+        {
+            string? aLine = null;
+            string? iLang = null;
+            string? oLang = null;
+            bool shouldRun = true;
 
-            parseJson(result);
 
-            Console.WriteLine("Want to translate another line? (Y)es/(N)o:");
-
-            bool continueCheck = true;
 
             do
             {
-                string? keepTranslating = Console.ReadLine();
-                if (keepTranslating == "Y" || keepTranslating == "y")
-                {
-                    continueCheck = false;
-                }
+                Console.WriteLine("Enter 2 character language code for languate to translate from:");
+                iLang = Console.ReadLine();
 
-                else if (keepTranslating == "N" || keepTranslating == "n")
-                {
-                    shouldRun = false;
-                    continueCheck = false;
-                }
+                Console.WriteLine("Enter 2 character language code for languate to translate to:");
+                oLang = Console.ReadLine();
 
-                else
+                Console.WriteLine("Enter Text to be translated:");
+                aLine = Console.ReadLine();
+                Console.WriteLine("Translating...");
+
+                SendText translator = new SendText();
+
+                string result = await translator.Translate(aLine, iLang, oLang);
+
+                parseJson(result);
+
+                Console.WriteLine("Want to translate another line? (Y)es/(N)o:");
+
+                bool continueCheck = true;
+
+                do
                 {
-                    Console.WriteLine("Please enter a valid character!");
+                    string? keepTranslating = Console.ReadLine();
+                    if (keepTranslating == "Y" || keepTranslating == "y")
+                    {
+                        continueCheck = false;
+                    }
+
+                    else if (keepTranslating == "N" || keepTranslating == "n")
+                    {
+                        shouldRun = false;
+                        continueCheck = false;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid character!");
+                    }
                 }
+                while (continueCheck == true);
             }
-            while (continueCheck == true);
+            while (shouldRun == true);
         }
-        while (shouldRun == true);
-    }
 
+    }
 }
